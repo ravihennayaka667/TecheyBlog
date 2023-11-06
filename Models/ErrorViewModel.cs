1bfef75c1d655dedc78f7c698eb7ab075dbf4577
ravi hennayake <ravihennayaka@outlook.com>
2023-11-06 10:56:21 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TecheyBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
