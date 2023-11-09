e5f5a1f0f7cf530345d2cc90255f1bf073f596be
ravi hennayake <ravihennayaka@outlook.com>
2023-11-09 15:53:13 +0530

    updated controller

---
+++ Controllers/AdminTagController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TecheyBlog.Controllers
+{
+    public class AdminTagController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
